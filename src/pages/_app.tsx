b4c3c860cdd2018e732eced2cda71274dd3f9b0e
joy.hwan <joy.hwan@kakaocorp.com>
2024-05-30 08:45:36 +0900

    feat : viewport 위치 및 로직 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no' />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }

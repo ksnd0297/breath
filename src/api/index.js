db19b62c03a8f5cb9d0cea94223747938fb44bcb
joy.hwan <joy.hwan@kakaocorp.com>
2024-05-30 23:19:54 +0900

    feat : response 변경

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,8 @@ import fetch from "node-fetch";
 export default async function handler(request, response) {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const data = await res.json();
-  return response.status(200).json({data});
+  return response.status(200).json({
+    ...data,
+    T: 10,
+  });
 }

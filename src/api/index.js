1e0c6902cfc1e251d732573c2fbecbb16080ef18
joy.hwan <joy.hwan@kakaocorp.com>
2024-05-30 23:11:39 +0900

    feat : api 폴더 추가

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import fetch from "node-fetch";
+
+export default async function handler(request, response) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  return response.status(200).json({data});
+}

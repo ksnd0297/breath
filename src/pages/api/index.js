f9ca1a00e32ae832fc36b27d7e13fd755c87369b
joy.hwan <joy.hwan@kakaocorp.com>
2024-05-30 23:27:38 +0900

    feat : 외화 API 제작

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,10 +1,9 @@
 import fetch from "node-fetch";
 
 export default async function handler(request, response) {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const res = await fetch("https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=lKvuroHtj4Fh2aq842GxUW57gUmErxNj&data=AP01");
   const data = await res.json();
   return response.status(200).json({
-    ...data,
-    T: 10,
+    data,
   });
 }

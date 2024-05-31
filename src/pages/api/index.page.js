f9690d70ab52c392b657fdad2641b7aa55fb1925
joy.hwan <joy.hwan@kakaocorp.com>
2024-06-01 02:54:21 +0900

    feat :  reject 오류 제거 및 리액트 warn 변경

--- src/pages/api/index.page.js
+++ src/pages/api/index.page.js
@@ -1,9 +1,14 @@
 import fetch from "node-fetch";
 import {format, subDays} from "date-fns";
 import {wrapApiHandlerWithSentry} from "@sentry/nextjs";
+import https from "https";
 
 async function handler(request, response) {
   const yesterDay = format(subDays(new Date(), 1), "yyyyMMdd");
+
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
 
   const res = await fetch(`https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.FINANCIAL_AUTH_TOKEN}&data=AP01&searchdate=${yesterDay}`, {
     method: "GET",
@@ -11,6 +16,7 @@ async function handler(request, response) {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
+    agent: httpsAgent,
   });
   const data = await res.json();
   try {

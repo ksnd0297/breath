58f9fb8577c9f274749f8d8d1431c777b6e5f83b
joy.hwan <joy.hwan@kakaocorp.com>
2024-05-31 15:45:59 +0900

    feat : 환율 API 제작

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,7 +1,10 @@
 import fetch from "node-fetch";
+import {format, subDays} from "date-fns";
 
 export default async function handler(request, response) {
-  const res = await fetch("https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=lKvuroHtj4Fh2aq842GxUW57gUmErxNj&data=AP01");
+  const yesterDay = format(subDays(new Date(), 1), "yyyyMMdd");
+
+  const res = await fetch(`https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=lKvuroHtj4Fh2aq842GxUW57gUmErxNj&data=AP01&searchdate=${yesterDay}`);
   const data = await res.json();
   return response.status(200).json({
     data,
---
+++ src/pages/api/type.ts
@@ -0,0 +1,28 @@
+/**
+ * result : 조회 결과
+ * cur_unit : 통화코드
+ * cur_nm : 국가/통화명
+ * ttb : 전신환(송금) 받으실때
+ * tts : 전신환(송금) 보내실때
+ * deal_bas_r : 매매 기준율
+ * bkpr : 장부가격
+ * yy_efee_r : 년환가료율
+ * ten_dd_efee_r : 10일환가료율
+ * kftc_deal_bas_r : 서울외국환중개매매기준율
+ * kfrtc_bkpr : 서울외국환중개장부가격
+ */
+export type ForeignResponse = {
+  data: {
+  result?: number 
+  cur_unit?: string
+  cur_nm?: string
+  ttb?: string
+  tts?: string
+  deal_bas_r?: string
+  bkpr?: string
+  yy_efee_r?: string
+  ten_dd_efee_r?: string
+  kftc_deal_bas_r?: string
+  kftc_bkpr?: string
+  }[]
+}

9fe5f587fbc8722365e7972b806e8eb7751a48a4
joy.hwan <joy.hwan@kakaocorp.com>
2024-06-02 01:03:58 +0900

    feat : 컴포넌트 분리 및 리패치 로직 제작

---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { LoadingOutlined } from "@ant-design/icons";
+
+
+const Loading = () => {
+    return (
+        <LoadingOutlined className='text-xl' />
+    )
+}
+
+export default Loading;

cb88af3283bb9745038ddb442db3db2b78efd961
joy.hwan <joy.hwan@kakaocorp.com>
2024-06-02 02:05:37 +0900

    feat : 영업일인 경우에만 API 작동

--- src/pages/components/Foreign/index.tsx
+++ src/pages/components/Foreign/index.tsx
@@ -4,6 +4,7 @@ import Info from "./components/Info";
 import useForeign from "./hooks/useForeign";
 import Loading from "@/components/Loading";
 import Empty from "@/components/Empty";
+import { isWeekend } from "date-fns";
 
 interface Props {
     money: number;
@@ -12,17 +13,22 @@ interface Props {
 const Foreign = (props: Props) => {
     const {money} = props;
 
-    const {data, isFetching, refetch} = useForeign();
+    const {data, isFetching, refetch: foreignRefetch} = useForeign();
 
     const InfoList = () => data?.data.map((info, index) => (
-        <Info key={index} data={info} money={money} />
+        <Info key={`${index}-${info.cur_nm}`} data={info} money={money} />
     ))
+
+    const refetch = () => {
+        if(isWeekend(new Date())) return;
+        foreignRefetch();
+    }
 
     return (
         <Flex className='h-90 p-5' vertical gap={20} justify='center'>
             <Flex justify="space-between">
                 <p className='text-xl font-bold'>외화는 얼마나 벌었을까 ?</p>
-                <SyncOutlined className='text-base' onClick={() => refetch()}/>
+                <SyncOutlined className='text-base' onClick={refetch}/>
             </Flex>
             {isFetching ? <Loading /> : <Row gutter={[20, 20]}>
                 {data?.data.length === 0 ? <Empty /> : <InfoList />}

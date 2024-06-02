d8b64c161d840d5e926932f005623ceef5357339
joy.hwan <joy.hwan@kakaocorp.com>
2024-06-02 13:25:50 +0900

    feat : date 유틸함수 제작 "yyyy-MM-dd" 형태의 포맷을 만들어주는 유틸함수 제작

---
+++ src/utils/date.ts
@@ -0,0 +1,11 @@
+import { format } from "date-fns"
+
+
+/**
+ * 
+ * @param date 
+ * @returns date "yyyy-MM-dd"
+ */
+export const formatDateWithoutTime = (date: Date | string | number) => {
+    return format(date, "yyyy-MM-dd")
+}

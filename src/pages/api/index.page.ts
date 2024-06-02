2666303ce8c35f2861bfcdcd3e1efb40d9b0fdd2
joy.hwan <joy.hwan@kakaocorp.com>
2024-06-02 13:26:32 +0900

    feat : searchDate 로직 변경 오늘이 주말일 경우 이전 가까운 금요일 조회하도록 변경

--- src/pages/api/index.page.ts
+++ src/pages/api/index.page.ts
@@ -1,17 +1,19 @@
-import {format} from "date-fns";
+import { isWeekend, previousFriday} from "date-fns";
 import { wrapApiHandlerWithSentry} from "@sentry/nextjs";
 import https from "https";
 import {NextApiRequest, NextApiResponse} from "next";
 import fetch from "node-fetch";
+import { formatDateWithoutTime } from "@/utils/date";
 
 async function handler(req: NextApiRequest, response: NextApiResponse) {
-    const today = format(new Date(), "yyyy-MM-dd");
 
     const httpsAgent = new https.Agent({
         rejectUnauthorized: false,
     });
 
-    const res = await fetch(`https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.FINANCIAL_AUTH_TOKEN}&data=AP01&searchdate=${today}`, {
+    const searchDate = isWeekend(new Date()) ? formatDateWithoutTime(previousFriday(new Date())) :formatDateWithoutTime(new Date());
+
+    const res = await fetch(`https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.FINANCIAL_AUTH_TOKEN}&data=AP01&searchdate=${searchDate}`, {
         method: "GET",
         headers: {
             Accept: "application/json",
